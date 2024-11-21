7a85dbfbc90676353014c6494b46db4c6034e1b8
Kevin-BV <Kevin.basoalto.v@gmail.com>
2024-11-21 20:31:12 -0300

    Pj animaciones correr saltar, idle

--- Assets/Scripts/Personaje/MovimientoPersonaje.cs
+++ Assets/Scripts/Personaje/MovimientoPersonaje.cs
@@ -9,11 +9,15 @@ public class MovimientoPersonaje : MonoBehaviour
     public float fuerzaDeSalto = 5f;
     private bool enSuelo = true; // Variable para verificar si está en el suelo
     private Rigidbody rb;
+    private Animator anim;
 
     void Start()
     {
         // Obtenemos el Rigidbody para aplicar la física al personaje
         rb = GetComponent<Rigidbody>();
+
+        // Obtenemos el Animator para controlar las animaciones
+        anim = GetComponent<Animator>();
     }
 
     void Update()
@@ -23,17 +27,35 @@ public class MovimientoPersonaje : MonoBehaviour
         float movimientoVertical = Input.GetAxis("Vertical");
 
         // Creamos el vector de movimiento
-        Vector3 movimiento = new Vector3(movimientoHorizontal, 0, movimientoVertical) * velocidad * Time.deltaTime;
+        Vector3 movimiento = new Vector3(movimientoHorizontal, 0, movimientoVertical);
 
-        // Movemos el personaje
-        transform.Translate(movimiento, Space.World);
+        // Movemos al personaje si hay movimiento
+        if (movimiento.magnitude > 0.1f) // Verificamos si el personaje se está moviendo
+        {
+            anim.SetBool("Correr", true); // Activamos la animación de correr
+
+            // Volteamos el personaje según la dirección del movimiento horizontal
+            if (movimientoHorizontal != 0)
+            {
+                Vector3 escala = transform.localScale;
+                escala.x = Mathf.Sign(movimientoHorizontal) * Mathf.Abs(escala.x); // Voltear en X
+                transform.localScale = escala;
+            }
+
+            // Movemos el personaje
+            transform.Translate(movimiento.normalized * velocidad * Time.deltaTime, Space.World);
+        }
+        else
+        {
+            anim.SetBool("Correr", false); // Detenemos la animación de correr
+        }
 
         // Detectamos la entrada de salto y verificamos si está en el suelo
         if (Input.GetButtonDown("Jump") && enSuelo)
         {
-            // Aplicamos fuerza en el eje Y para saltar
-            rb.AddForce(Vector3.up * fuerzaDeSalto, ForceMode.Impulse);
-            enSuelo = false; // Indicamos que el personaje ya no está en el suelo
+            anim.SetBool("Saltar", true); // Activamos la animación de salto
+            rb.AddForce(Vector3.up * fuerzaDeSalto, ForceMode.Impulse); // Aplicamos fuerza para saltar
+            enSuelo = false; // Indicamos que ya no está en el suelo
         }
     }
 
@@ -43,6 +65,7 @@ public class MovimientoPersonaje : MonoBehaviour
         if (colision.gameObject.CompareTag("Suelo"))
         {
             enSuelo = true; // Indicamos que está en el suelo
+            anim.SetBool("Saltar", false); // Detenemos la animación de salto
         }
     }
 }
